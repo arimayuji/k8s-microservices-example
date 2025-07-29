f1ed80cc7456955969eb1b5d440e25e367f43626
Yuji Arima <yujiarima@hotmail.com>
2025-07-29 11:09:36 -0300

    feat: adds user controller

---
+++ user-service/src/controllers/user.controller.ts
@@ -0,0 +1,30 @@
+import { User } from "../models/user.model";
+import { IUserRepository } from "../repositories/user.repository.mock";
+
+export class UserController {
+  constructor(private readonly repository: IUserRepository) { }
+  
+  async createUser(user: User): Promise<User> {
+    const createdUser = await this.repository.createUser(user);
+    
+    return createdUser;
+  }
+
+  async findUser(id: string): Promise<User | null> {
+    const user = await this.repository.findById(id);
+    
+    return user;
+  }
+
+  async updateUser(id: string, user: Partial<User>): Promise<User | null> {
+    const updatedUser = await this.repository.updateUser(id, user);
+
+    return updatedUser;
+  }
+
+  async deleteUser(id: string): Promise<void> {
+    await this.repository.deleteUser(id);
+    
+    return Promise.resolve();
+  }
+}
