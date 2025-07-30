d63e5f55fbd3255daf8baf4d767dc7744838a253
Yuji Arima <yujiarima@hotmail.com>
2025-07-30 09:25:03 -0300

    chore: adds app.ts in auth-service

---
+++ auth-service/src/app.ts
@@ -0,0 +1,48 @@
+import fastify from "fastify";
+import { jsonSchemaTransform, ZodTypeProvider } from "fastify-type-provider-zod";
+import { InMemoryRepository } from "./repositories/auth.repository.mock";
+import { authRoutes } from "./routes/auth.routes";
+import { AuthController } from "./controllers/auth.controller";
+import { AuthService } from "./services/auth.service";
+import { fastifySwagger } from "@fastify/swagger";
+
+const app = fastify({
+  logger: true,
+}).withTypeProvider<ZodTypeProvider>();
+
+const PORT = 8081;
+
+const start = async () => {
+  try {
+    await app.register(fastifySwagger, {
+      openapi: {
+        info: {
+          title: "Whatlas API",
+          version: "1.0.0",
+        },
+        servers: [{ url: `http://localhost:${process.env.PORT || 8081}` }],
+      },
+      transform: jsonSchemaTransform,
+     });
+    
+    await app.register((instance, opts, done) => {
+      const repository = new InMemoryRepository();
+      const service = new AuthService(repository);
+      const controller = new AuthController(service);
+
+      authRoutes(instance, controller)
+      
+      done();
+    })
+
+    await app.listen({ port: PORT, host: "0.0.0.0" });
+    app.log.info(`🚀 Server running on port ${PORT}`);
+
+    await app.ready();
+  } catch (err) {
+    app.log.error("Failed to start server:", err);
+    process.exit(1);
+  }
+};
+
+start();
