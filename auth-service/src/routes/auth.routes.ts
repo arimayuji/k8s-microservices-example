6c315c16397621bec3b9879071c4eec4b2bc71e8
Yuji Arima <yujiarima@hotmail.com>
2025-07-31 08:28:24 -0300

    chore(auth): adds checks routes for auth.routes

--- auth-service/src/routes/auth.routes.ts
+++ auth-service/src/routes/auth.routes.ts
@@ -4,7 +4,7 @@ import { AuthController } from "../controllers/auth.controller";
 
 export async function authRoutes(app: FastifyTypedInstance, controller: AuthController) {
   
-  app.post('/auth/login', async (request: FastifyRequest<{Body: { email: string; password: string }}>, reply) => {
+  app.post('/auth/login', async (request: FastifyRequest<{ Body: { email: string; password: string } }>, reply) => {
     try {
       const { email, password } = request.body;
 
@@ -64,6 +64,33 @@ export async function authRoutes(app: FastifyTypedInstance, controller: AuthCont
       reply.send({ message: result });
     } catch (error) {
       reply.status(500).send({ error: "Erro ao resetar senha" });
+    }
+  });
+
+  app.get("/auth/healtz", async (request, reply) => {
+    try {
+      const healthStatus = await controller.healthCheck();
+      reply.send({ status: healthStatus });
+    } catch (error) {
+      reply.status(500).send({ error: "Erro no serviço de autenticação" });
+    }
+  });
+
+  app.get("/auth/readyz", async (request, reply) => {
+    try {
+      const readyStatus = await controller.readyCheck();
+      reply.send({ ready: readyStatus });
+    } catch (error) {
+      reply.status(500).send({ error: "Erro no serviço de autenticação" });
+    }
+  })
+
+  app.get("/auth/startupz", async (request, reply) => {
+    try {
+      const startupStatus = await controller.startupCheck();
+      reply.send({ startup: startupStatus });
+    } catch (error) {
+      reply.status(500).send({ error: "Erro no serviço de autenticação" });
     }
   });
 }
